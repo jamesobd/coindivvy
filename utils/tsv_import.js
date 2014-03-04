c004acb7d8185811e3e51a43a6d7135539bc8eb2
James <james@obdstudios.com>
2014-03-03 21:57:14 -0700

    tocsv updated

--- utils/tsv_import.js
+++ utils/tsv_import.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var mongoose = require('../node_modules/mongoose');
+var _ = require('underscore');
 
 // Load Models
 mongoose.connect('mongodb://localhost/coindivvy');
@@ -17,17 +18,17 @@ var accountSettings = {
 
 // Process the csv file and collect the accounts/addresses synchronously first
 var accounts = {};
-var lines = fs.readFileSync(__dirname + '/csv.csv', 'utf8').split('\n');
+var lines = fs.readFileSync(__dirname + '/import.tsv', 'utf8').split('\n');
 lines.forEach(function (line, i, lines) {
     if (i != 0) {
         // Parse out the current line
-        var values = line.split(',');
-        if (values.length != 4) return;
+        var values = line.split('\t');
+        if (values.length != 4) return console.error("File contained invalid line: " + line);
 
         var accountName = values[0];
-        var address = {_id: values[1], units: values[2], notes: values[3]};
+        var address = {_id: values[1], units: values[2].replace(/\,/g, ''), notes: values[3]};
 
-        // If the account is not in the accounts object then add it.
+        // If the account is not in the accounts object then add it
         if (accounts[accountSettings.owner + "_" + accountName] === undefined) {
             accounts[accountSettings.owner + "_" + accountName] = {
                 _id: accountSettings.owner + "_" + accountName,
@@ -40,8 +41,14 @@ lines.forEach(function (line, i, lines) {
                 unit_name: accountSettings.unit_name
             };
         }
-        // Add the address
-        accounts[accountSettings.owner + "_" + accountName]['addresses'].push(address);
+
+        // If the address already exists update it, otherwise add it
+        var existingAddress = _.findWhere(accounts[accountSettings.owner + "_" + accountName]['addresses'], {_id: address._id});
+        if (existingAddress !== undefined) {
+            _.extend(existingAddress, address);
+        } else {
+            accounts[accountSettings.owner + "_" + accountName]['addresses'].push(address);
+        }
     }
 });
 
@@ -54,10 +61,27 @@ for (var accountName in accounts) {
 
             // If the account is not in the database yet save the new account
             if (accountDoc === null) {
-                new Account(account).save(function (err, accountDoc, numberAffected) {
-                    if (err) return console.error("Trying to create the account", err, accountDoc, numberAffected);
+                var coin = require('../coins/' + 'bitcoin');
 
-                    console.log("Account created: " + accountDoc._id);
+                coin.client.listAccounts(function (err, walletAccounts) {
+
+                    // Create a new bitcoin account and save it's address
+                    if (walletAccounts.hasOwnProperty(account._id)) {
+                        return console.error('The account "' + account._id + '" is already part of the wallet but is missing in the database.');
+                    }
+
+                    // Create a new wallet account and address
+                    coin.client.getNewAddress(account._id, function (err, newAddress) {
+                        if (err) return console.error(err, address);
+                        account.address = newAddress;
+
+                        // Save the new account to the database
+                        new Account(account).save(function (err, accountDoc, numberAffected) {
+                            if (err) return console.error("Trying to create the account", err, accountDoc, numberAffected);
+
+                            console.log("Account created: " + accountDoc._id);
+                        });
+                    });
                 });
             } else {
                 // Upsert all addresses into the account document
@@ -83,7 +107,6 @@ for (var accountName in accounts) {
                     console.log('Account updated: ' + accountDoc._id);
                 });
             }
-
         });
     })(accounts[accountName])
 }
