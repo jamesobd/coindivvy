edfbad942b3a6939525e0352b4a7c9ee73f37f87
James <james@obdstudios.com>
2014-03-05 21:13:01 -0700

    test

--- test.js
+++ test.js
@@ -5,9 +5,3 @@ var coin = require('./coins/' + 'bitcoin');
 coin.client.listAccounts(function (err, accounts) {
     console.log(err, accounts);
 });
-
-Account.findById('delmonger_Coin Auger', function (err, accountDoc) {
-    if (err) return console.error(err, accountDoc);
-
-    console.log(accountDoc);
-});
