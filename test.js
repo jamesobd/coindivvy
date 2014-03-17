cd3586a546daa6138b13eab611a003e5dd4b7248
James <james@obdstudios.com>
2014-03-17 08:52:04 -0600

    Test gets balance

--- test.js
+++ test.js
@@ -8,5 +8,9 @@ var Account = require('./models/account.js');
 
 var coin = require('./coins/' + 'bitcoin');
 coin.client.listAccounts(function (err, accounts) {
-    console.log(err, accounts);
+    console.log(accounts);
+
+    coin.client.getbalance(function (err, balance) {
+        console.log(balance);
+    });
 });
