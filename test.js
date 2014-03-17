b5d521219a261bb770a7f55ee1c3c738a1c1466d
James <james@obdstudios.com>
2014-03-17 10:31:56 -0600

    Test gets balance

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ var coin = require('./coins/' + 'bitcoin');
 coin.client.listAccounts(coin.minConfirmations, function (err, accounts) {
     console.log(accounts);
 
-    coin.client.getBalance(coin.minConfirmations, function (err, balance) {
+    coin.client.getBalance('*', coin.minConfirmations, function (err, balance) {
         console.log(balance);
     });
 });
